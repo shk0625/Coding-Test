34427dd37cca9d2e1973ba286e2f3ca7e995ff6e
shk0625 <tjfgml1238@gmail.com>
2023-07-14 15:34:17 +0900

    n_15894 answer

---
+++ src/Beakjoon/n_15894/Main.java
@@ -0,0 +1,11 @@
+package Beakjoon.n_15894;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Long result = sc.nextLong();
+        System.out.println(result*4);
+    }
+}
