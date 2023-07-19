a11c604b5709392d6340ec9786af6faef887ab8a
shk0625 <tjfgml1238@gmail.com>
2023-07-20 00:43:55 +0900

    n_2562 answer

--- src/Beakjoon/n_2562/Main.java
+++ src/Beakjoon/n_2562/Main.java
@@ -1,4 +1,21 @@
 package Beakjoon.n_2562;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        int[] getNum = new int[9];
+        int max = Integer.MIN_VALUE;;
+        int arraylocation = 0;
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i< getNum.length; i++) {
+            getNum[i] = sc.nextInt();
+            if(getNum[i]>max){
+                max = getNum[i];
+                arraylocation = i;
+            }
+        }
+        System.out.println(max);
+        System.out.println(arraylocation+1);
+    }
 }
